84bb80cd373ab96ca54fcbc95e0179c1709dd678
skellik-1 <82837093+skellik-1@users.noreply.github.com>
2021-05-27 20:24:15 +0300

    генерация

--- app/Views/admin/profile.php
+++ app/Views/admin/profile.php
@@ -14,11 +14,18 @@
                             </div>
                         </div>
                         <div class="form-group">
+
                             <label class="form-label" for="email-address">Пароль</label>
                             <div class="form-control-wrap">
-                                <input type="text" class="form-control" name="password" >
+                                <div class="input-group">
+                                <input type="text" class="form-control" name="password" id="pass" style="border-right: 0px;">
+                            <div class="input-group-append">
+                               <a onclick="gen_password();" style="cursor: pointer;"> <span class="input-group-text" style="background-color: white;border-left: 0px;">&</span></a>
                             </div>
                         </div>
+                    </div>
+                    </div>
+
                          <div class="form-group">
                             <label class="form-label" for="email-address">Баланс</label>
                             <div class="form-control-wrap">
@@ -36,3 +43,11 @@
  </div>  
    </div>  
      </div>    
+<script type="text/javascript">
+    
+function gen_password(){
+    $("#pass").val(Math.random().toString(36).slice(-8)) ;
+}
+
+
+</script>
